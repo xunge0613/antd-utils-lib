9b0f7a940f57944377708e07b10e86cefcdd6886
dalston <xxdalston@gmail.com>
2020-06-11 16:35:57 +0800

    feat: add enum2option

--- src/entry-compiler.ts
+++ src/entry-compiler.ts
@@ -8,7 +8,7 @@ import transformTable from './table/transform-table';
 
 // select
 import pasteTags from './select/paste-tags';
-import enum2Options from './select/enum2option';
+import enum2Options from './select/enum2options';
 
 export = {
   transformTable,
